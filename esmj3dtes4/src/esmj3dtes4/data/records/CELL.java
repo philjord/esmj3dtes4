775662470b5d3d5ee732cb7b880808c6ed47c357
philip <philip@ihug.co.nz>
2013-09-08 18:14:21 +1200

    .

--- esmj3dtes4/src/esmj3dtes4/data/records/CELL.java
+++ esmj3dtes4/src/esmj3dtes4/data/records/CELL.java
@@ -5,36 +5,18 @@ import java.util.ArrayList;
 import tools.io.ESMByteConvert;
 import esmLoader.common.data.record.Record;
 import esmLoader.common.data.record.Subrecord;
-import esmj3d.data.shared.records.InstRECO;
-import esmj3d.data.shared.subrecords.FormID;
-import esmj3d.data.shared.subrecords.LString;
-import esmj3d.data.shared.subrecords.ZString;
+import esmj3d.data.shared.records.CommonCELL;
 
-public class CELL extends InstRECO
+public class CELL extends CommonCELL
 {
-	public ZString EDID;
-
-	public LString FULL;
 
 	public DATA DATA;
 
 	public byte musicType = 1;
 
-	public int XOWN = -1;
-
 	public int XGLB = -1;
 
-	public int XCCM = -1;
-
-	public int XCWT = -1;
-
 	public int XRNK = 0;
-
-	public byte[] XCLL = null; //Lighting for interior cell
-
-	public FormID[] XCLRs; // array of region ids
-
-	public float XCLW = 0; //water height if not 0
 
 	public CELL(Record recordData)
 	{
@@ -46,44 +28,9 @@ public class CELL extends InstRECO
 			Subrecord sr = subrecords.get(i);
 			byte[] bs = sr.getSubrecordData();
 
-			ArrayList<FormID> XCLRsl = new ArrayList<FormID>();
-
-			if (sr.getSubrecordType().equals("EDID"))
-			{
-				EDID = new ZString(bs);
-			}
-			else if (sr.getSubrecordType().equals("FULL"))
-			{
-				FULL = new LString(bs);
-			}
-			else if (sr.getSubrecordType().equals("DATA"))
+			if (sr.getSubrecordType().equals("DATA"))
 			{
 				DATA = new DATA(bs);
-			}
-			else if (sr.getSubrecordType().equals("XCLL"))
-			{
-				XCLL = bs;
-			}
-			else if (sr.getSubrecordType().equals("XCLC"))
-			{
-				x = ESMByteConvert.extractInt(bs, 0);
-				y = ESMByteConvert.extractInt(bs, 4);
-			}
-			else if (sr.getSubrecordType().equals("XCLR"))
-			{
-				XCLRsl.add(new FormID(bs));
-			}
-			else if (sr.getSubrecordType().equals("XCLW"))
-			{
-				XCLW = ESMByteConvert.extractFloat(bs, 0);
-			}
-			else if (sr.getSubrecordType().equals("XCWT"))
-			{
-				XCWT = ESMByteConvert.extractInt(bs, 0);
-			}
-			else if (sr.getSubrecordType().equals("XOWN"))
-			{
-				XOWN = ESMByteConvert.extractInt(bs, 0);
 			}
 			else if (sr.getSubrecordType().equals("XCMT"))
 			{
@@ -93,21 +40,16 @@ public class CELL extends InstRECO
 			{
 				XRNK = ESMByteConvert.extractInt(bs, 0);
 			}
-			else if (sr.getSubrecordType().equals("XCCM"))
-			{
-
-			}
 			else if (sr.getSubrecordType().equals("XGLB"))
 			{
 
 			}
 			else
 			{
-				System.out.println("unhandled : " + sr.getSubrecordType() + " in " + recordData);
+				// no longer possible with CommonCELL
+				//System.out.println("unhandled : " + sr.getSubrecordType() + " in " + recordData);
 			}
 
-			XCLRs = new FormID[XCLRsl.size()];
-			XCLRsl.toArray(XCLRs);
 		}
 	}
 
