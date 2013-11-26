61d40a3943319c7a4b07f5a45f2ea2182693d3be
philip <philip@ihug.co.nz>
2013-11-27 10:59:25 +1300

    containers work

--- esmj3dtes4/src/esmj3dtes4/data/records/CONT.java
+++ esmj3dtes4/src/esmj3dtes4/data/records/CONT.java
@@ -2,35 +2,19 @@ package esmj3dtes4.data.records;
 
 import java.util.ArrayList;
 
-import tools.io.ESMByteConvert;
 import esmLoader.common.data.record.Record;
 import esmLoader.common.data.record.Subrecord;
-import esmj3d.data.shared.records.RECO;
-import esmj3d.data.shared.subrecords.CNTO;
+import esmj3d.data.shared.records.GenericCONT;
 import esmj3d.data.shared.subrecords.FormID;
 import esmj3d.data.shared.subrecords.LString;
-import esmj3d.data.shared.subrecords.MODL;
-import esmj3d.data.shared.subrecords.ZString;
 
-public class CONT extends RECO
+public class CONT extends GenericCONT
 {
-	public ZString EDID;
-
 	public LString FULL;
 
 	public DATA DATA;
 
 	public FormID SCRI;
-
-	public MODL MODL;
-
-	 
-
-	public CNTO[] CNTOs = null;
-
-	public int SNAM;
-
-	public int QNAM;
 
 	public byte[] MNAM = null;
 
@@ -45,13 +29,7 @@ public class CONT extends RECO
 			Subrecord sr = subrecords.get(i);
 			byte[] bs = sr.getSubrecordData();
 
-			ArrayList<CNTO> CNTOsl = new ArrayList<CNTO>();
-
-			if (sr.getSubrecordType().equals("EDID"))
-			{
-				EDID = new ZString(bs);
-			}
-			else if (sr.getSubrecordType().equals("FULL"))
+			  if (sr.getSubrecordType().equals("FULL"))
 			{
 				FULL = new LString(bs);
 			}
@@ -62,50 +40,20 @@ public class CONT extends RECO
 			else if (sr.getSubrecordType().equals("SCRI"))
 			{
 				SCRI = new FormID(bs);
-			}
-			else if (sr.getSubrecordType().equals("MODL"))
-			{
-				MODL = new MODL(bs);
-			}
-			else if (sr.getSubrecordType().equals("MODB"))
-			{
-				MODL.addMODBSub(bs);
-			}
-			else if (sr.getSubrecordType().equals("MODT"))
-			{
-				MODL.addMODTSub(bs);
-			}
+			}			 
 			else if (sr.getSubrecordType().equals("MNAM"))
 			{
 				MNAM = bs;
-			}
-			else if (sr.getSubrecordType().equals("SNAM"))
-			{
-				SNAM = ESMByteConvert.extractInt(bs, 0);
-			}
-			else if (sr.getSubrecordType().equals("QNAM"))
-			{
-				QNAM = ESMByteConvert.extractInt(bs, 0);
-			}
-			else if (sr.getSubrecordType().equals("CNTO"))
-			{
-				CNTOsl.add(new CNTO(bs));
-			}
+			}			 
 			else
 			{
-				System.out.println("unhandled : " + sr.getSubrecordType() + " in record " + recordData + " in " + this);
+				//System.out.println("unhandled : " + sr.getSubrecordType() + " in record " + recordData + " in " + this);
 			}
 
-			CNTOs = new CNTO[CNTOsl.size()];
-			CNTOsl.toArray(CNTOs);
 		}
 	}
 
-	public String showDetails()
-	{
-		return "CONT : (" + formId + "|" + Integer.toHexString(formId) + ") " + EDID.str + " : " + MODL.model;
-	}
-
+	
 	public class DATA
 	{
 		public byte[] data;
@@ -115,4 +63,6 @@ public class CONT extends RECO
 			data = bytes;
 		}
 	}
+
+	
 }
