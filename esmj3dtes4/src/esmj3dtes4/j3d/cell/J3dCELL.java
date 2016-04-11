af71d5824cee55ce88d8fccb2097adebdb577c06
phil <philjord@ihug.co.nz>
2016-04-11 16:53:42 +0200

    removed prebaking location into LAND

--- esmj3dtes4/src/esmj3dtes4/j3d/cell/J3dCELL.java
+++ esmj3dtes4/src/esmj3dtes4/j3d/cell/J3dCELL.java
@@ -143,20 +143,20 @@ public abstract class J3dCELL extends J3dCELLGeneral implements UpdateListener
 					Record parentLANDrec = ((J3dCellFactory) master).getParentWRLDLAND(worldId, (int) instCell.getTrans().x,
 							(int) instCell.getTrans().y);
 					if (parentLANDrec != null)
-						j3dLAND = new J3dLAND(new LAND(parentLANDrec), cellLocation);
+						j3dLAND = new J3dLAND(new LAND(parentLANDrec));
 					else
-						j3dLAND = new J3dLAND(new LAND(record), cellLocation);
+						j3dLAND = new J3dLAND(new LAND(record));
 				}
 				else
 				{
 					Record parentLANDrec = ((J3dCellFactory) master).getParentWRLDLAND(worldId, (int) instCell.getTrans().x,
 							(int) instCell.getTrans().y);
 					if (parentLANDrec != null)
-						j3dLAND = new J3dLAND(new LAND(parentLANDrec), master, mediaSources.getTextureSource(), cellLocation);
+						j3dLAND = new J3dLAND(new LAND(parentLANDrec), master, mediaSources.getTextureSource());
 					else
-						j3dLAND = new J3dLAND(new LAND(record), master, mediaSources.getTextureSource(), cellLocation);
+						j3dLAND = new J3dLAND(new LAND(record), master, mediaSources.getTextureSource());
 				}
-
+				j3dLAND.setLocation(cellLocation);
 				ret = j3dLAND;
 			}
 			else
